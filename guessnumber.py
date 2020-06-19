171e50e3770e2f0a293da30285b0c9ea25a28a7e
Meghana Yelamarthi <33190366+ymeghana@users.noreply.github.com>
2020-06-19 22:15:41 +0530

    Guess Number

---
+++ guessnumber.py
@@ -0,0 +1,47 @@
+import random
+#GET GUESS
+def get_guess():
+    return list(input("WHat is your guess!!"))
+#GENERATE COMPUTER CODE 123
+def generate_code():
+    digits = [str(num) for num in range(10)]
+
+    #Shuffle the digits
+    random.shuffle(digits)
+    #then grab the 1st three
+    return digits[:3]
+
+#GENERATE THE CLUES
+
+def generate_clues(code,user_guess):
+
+    if user_guess == code:
+        return "CODE CRACKED!!"
+    clues = []
+
+    for ind,num in enumerate(user_guess):
+        if num == code[ind]:
+            clues.append("match")
+        elif num in code:
+            clues.append("Close")
+
+    if clues == []:
+        return ["Nope"]
+    else:
+        return clues
+
+#RUN GAME LOGIC
+print("Welcome Code Breaker!!")
+
+secret_code = generate_code()
+
+clue_report =[]
+
+while clue_report != "CODE CRACKED!!":
+
+    guess = get_guess()
+
+    clue_report = generate_clues(guess,secret_code)
+    print("here is the result of your guess: ")
+    for clue in clue_report:
+        print(clue)
